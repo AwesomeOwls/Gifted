bce4545df6752af5bb50c79a7b668e67c007ef0c
Yehonatan Sayag <sayag11@gmail.com>
2017-04-27 21:04:24 +0300

    Google Auth added + login buttons + more minor changes

---
+++ gifted-ui/views/intro/IntroView.js
@@ -0,0 +1,51 @@
+// import $ from 'gifted-ui/flatfy-ui/js/jquery-1.10.2.js'
+
+function startApp() {
+    $('document').ready(function () {
+        var user = null;
+        $('#search-button > a').click(function () {
+            // $('#about').hide();
+        });
+
+        $('#home-button').click(function () {
+            // $('#about').show();
+        });
+    });
+}
+
+var GoogleAuth = {
+
+    onSignIn: function (googleUser) {
+
+    var profile = googleUser.getBasicProfile();
+    $('#login-button').hide();
+    $('#logout-button').show();
+    $('#logout-button').click(GoogleAuth.signOut);
+    //TODO check if user is in DB Already.
+    $('#welcome').show();
+    $('#welcome')[0].innerText = 'Welcome, ' + profile.getGivenName();
+},
+
+    signOut: function () {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(() => {
+        $('#logout-button').hide();
+        $('#login-button').find('a')[0].innerText = 'Log In';
+        $('#login-button').show();
+        $('#welcome')[0].innerText = '';
+        $('#welcome').hide();
+        GoogleAuth.setLoginButton();
+    });
+},
+    setLoginButton: function() {
+        auth2.attachClickHandler($('#login-button')[0], {},
+            GoogleAuth.onSignIn, function(error) {
+                alert(JSON.stringify(error, undefined, 2));
+            });
+    }
+
+
+};
+
+
+startApp();
