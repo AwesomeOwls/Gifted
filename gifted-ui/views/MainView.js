a3c6350a1300fed3353a0baebd7731ba3ac1769e
yotam ronen <yotronen@gmail.com>
2017-05-06 21:35:57 +0300

    login loading improved + search/upload modals initial code + w3 html injection imported

--- gifted-ui/views/MainView.js
+++ gifted-ui/views/MainView.js
@@ -3,14 +3,14 @@
 var GoogleAuth = {
 
     onSignIn: function (googleUser) {
-    var $logout = $('#logout-button'); var $body = $('body');
-    var $status = $('#status'); var $preloader = $('#preloader');
+    var $logout = $('#logout-button'); var $body = $('body'); var $search = $('#search-button');
+    var $status = $('#status'); var $preloader = $('#preloader'); var $upload = $('#upload-button');
 
     console.log('user logged in');
     var profile = googleUser.getBasicProfile();
     var pictureURL = profile.getImageUrl();
     $logout.click(GoogleAuth.signOut);
-
+    NavBar.hideAllButtons();
     $status.show();
     $preloader.show();
     $status.delay(300).fadeOut();
@@ -18,13 +18,16 @@ var GoogleAuth = {
         $body.delay(550).css({'overflow':'visible'});
         NavBar.showTopButtons();
         NavBar.showWelcome(profile.getGivenName(), pictureURL);
+
+        $search.click(function() {$('#search-modal').modal() });
+        $upload.click(function() {$('#upload-modal').modal() });
     });
 },
 
     signOut: function () {
         var $body = $('body'); var $status = $('#status');
         var $preloader = $('#preloader');
-
+        NavBar.hideAllButtons();
         var auth2 = gapi.auth2.getAuthInstance();
         auth2.signOut().then(function() {
             console.log('user logged out');
@@ -50,25 +53,45 @@ var NavBar = {
     hideTopButtons: function() {
         var $login = $('#login-button'); var $logout = $('#logout-button'); var $welcome = $('#welcome');
         var $search = $('#search-button'); var $upload = $('#upload-button'); var $account = $('#account-button');
+        var $about = $('#about-button');
+
 
         $logout.hide();
         $search.hide();
         $upload.hide();
         $account.hide();
         $login.show();
+        $about.show();
         $welcome.hide();
+        $welcome[0].innerText = '';
+    },
+
+    hideAllButtons: function() {
+        var $login = $('#login-button'); var $logout = $('#logout-button'); var $welcome = $('#welcome');
+        var $search = $('#search-button'); var $upload = $('#upload-button'); var $account = $('#account-button');
+        var $about = $('#about-button');
+
+        $logout.hide();
+        $search.hide();
+        $upload.hide();
+        $account.hide();
+        $login.hide();
+        $welcome.hide();
+        $about.hide();
         $welcome[0].innerText = '';
     },
 
     showTopButtons: function() {
         var $login = $('#login-button'); var $logout = $('#logout-button');
         var $search = $('#search-button'); var $upload = $('#upload-button'); var $account = $('#account-button');
+        var $about = $('#about-button');
 
         $login.hide();
         $logout.show();
         $search.show();
         $upload.show();
         $account.show();
+        $about.show();
     },
 
     showWelcome: function(userName, pictureURL) {
