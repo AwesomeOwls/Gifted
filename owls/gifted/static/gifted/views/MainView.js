ab5b1b9c4fafce145e944360d68a47019ad95c43
Yehonatan Sayag <sayag11@gmail.com>
2017-05-07 17:22:45 +0300

    more screwup fixes for dependencies - PLEASE DO NOT MESS WITH DEPENDENCIES AND PATHS WITHOUT INFORMING BEFOREHAND

--- owls/gifted/static/gifted/views/MainView.js
+++ owls/gifted/static/gifted/views/MainView.js
@@ -3,26 +3,26 @@
 var GoogleAuth = {
 
     onSignIn: function (googleUser) {
-    var $logout = $('#logout-button'); var $body = $('body'); var $search = $('#search-button');
-    var $status = $('#status'); var $preloader = $('#preloader'); var $upload = $('#upload-button');
+        var $logout = $('#logout-button'); var $body = $('body'); var $search = $('#search-button');
+        var $status = $('#status'); var $preloader = $('#preloader'); var $upload = $('#upload-button');
 
-    console.log('user logged in');
-    var profile = googleUser.getBasicProfile();
-    var pictureURL = profile.getImageUrl();
-    $logout.click(GoogleAuth.signOut);
-    NavBar.hideAllButtons();
-    $status.show();
-    $preloader.show();
-    $status.delay(300).fadeOut();
-    $preloader.delay(300).fadeOut('slow', function() {
-        $body.delay(550).css({'overflow':'visible'});
-        NavBar.showTopButtons();
-        NavBar.showWelcome(profile.getGivenName(), pictureURL);
+        console.log('user logged in');
+        var profile = googleUser.getBasicProfile();
+        var pictureURL = profile.getImageUrl();
+        $logout.click(GoogleAuth.signOut);
+        NavBar.hideAllButtons();
+        $status.show();
+        $preloader.show();
+        $status.delay(300).fadeOut();
+        $preloader.delay(300).fadeOut('slow', function() {
+            $body.delay(550).css({'overflow':'visible'});
+            NavBar.showTopButtons();
+            NavBar.showWelcome(profile.getGivenName(), pictureURL);
 
-        $search.click(function() {$('#search-modal').modal() });
-        $upload.click(function() {$('#upload-modal').modal() });
-    });
-},
+            $search.click(function() {$('#search-modal').modal() });
+            $upload.click(function() {$('#upload-modal').modal() });
+        });
+    },
 
     signOut: function () {
         var $body = $('body'); var $status = $('#status');
