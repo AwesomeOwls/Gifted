cdfd8deae2761de244bc9b23d6bf509e191599e1
Amit <Amit@xirius.com>
2017-06-10 19:26:52 +0300

    added the beautiful FAQ page

--- owls/gifted/static/gifted/views/main/NavBar.js
+++ owls/gifted/static/gifted/views/main/NavBar.js
@@ -2,16 +2,22 @@
  * Created by ysayag on 09/05/2017.
  */
 var NavBar = {
-    setLoginButton: function() {
+    setLoginButton: function () {
         var $login = $('#login-button');
         auth2.attachClickHandler($login[0], {},
-            GoogleAuth.onSignIn, function(e) {console.error(e.error)});
+            GoogleAuth.onSignIn, function (e) {
+                console.error(e.error)
+            });
     },
 
-    hideTopButtons: function() {
-        var $login = $('#login-button'); var $logout = $('#logout-button'); var $welcome = $('#welcome');
-        var $search = $('#search-button'); var $upload = $('#upload-button');
+    hideTopButtons: function () {
+        var $login = $('#login-button');
+        var $logout = $('#logout-button');
+        var $welcome = $('#welcome');
+        var $search = $('#search-button');
+        var $upload = $('#upload-button');
         var $about = $('#about-button');
+        var $faq = $('#faq-button');
 
         $logout.hide();
         $logout.off('click');
@@ -19,15 +25,20 @@ var NavBar = {
         $upload.hide();
         $login.show();
         $about.show();
+        $faq.show();
         $welcome.hide();
         $welcome.off('click');
         $welcome[0].innerText = '';
     },
 
-    hideAllButtons: function() {
-        var $login = $('#login-button'); var $logout = $('#logout-button'); var $welcome = $('#welcome');
-        var $search = $('#search-button'); var $upload = $('#upload-button');
+    hideAllButtons: function () {
+        var $login = $('#login-button');
+        var $logout = $('#logout-button');
+        var $welcome = $('#welcome');
+        var $search = $('#search-button');
+        var $upload = $('#upload-button');
         var $about = $('#about-button');
+        var $faq = $('#faq-button');
 
         $logout.hide();
         $logout.off('click');
@@ -37,13 +48,18 @@ var NavBar = {
         $welcome.hide();
         $welcome.off('click');
         $about.hide();
+        $faq.hide();
+
         $welcome[0].innerText = '';
     },
 
-    showTopButtons: function() {
-        var $login = $('#login-button'); var $logout = $('#logout-button');
-        var $search = $('#search-button'); var $upload = $('#upload-button');
+    showTopButtons: function () {
+        var $login = $('#login-button');
+        var $logout = $('#logout-button');
+        var $search = $('#search-button');
+        var $upload = $('#upload-button');
         var $about = $('#about-button');
+        var $faq = $('#faq-button');
 
         $login.hide();
         $logout.show();
@@ -51,10 +67,13 @@ var NavBar = {
         $search.show();
         $upload.show();
         $about.show();
+        $faq.show();
     },
 
-    showWelcome: function(userName, pictureURL) {
-        var $status = $('#status'); var $preloader = $('#preloader'); var $body = $('body');
+    showWelcome: function (userName, pictureURL) {
+        var $status = $('#status');
+        var $preloader = $('#preloader');
+        var $body = $('body');
 
         var introHeader = 'static/gifted/inner-templates/introHeader.html';
         pictureURL = pictureURL.replace(/\"/g, "");
@@ -72,7 +91,7 @@ var NavBar = {
         rankBar.appendTo('#welcome');
         $welcome.show();
 
-        $welcome.click( function() {
+        $welcome.click(function () {
 
             var obj = {};
             obj['user_id'] = Utils.readCookie('user_id');
@@ -85,17 +104,17 @@ var NavBar = {
                 contentType: "application/json; charset=utf-8",
                 data: JSON.stringify(obj),
                 dataType: "json",
-                beforeSend: function(){
+                beforeSend: function () {
                     $status.show();
                     $preloader.show();
                 },
-                success: function(data){
+                success: function (data) {
                     $preloader.delay(300).fadeOut('slow', function () {
                         $body.delay(550).css({'overflow': 'visible'});
                         ProfileView.showProfilePage(data.gifts);
                     });
                 },
-                error: function(error){
+                error: function (error) {
                     $status.hide();
                     $preloader.hide();
                     errorDialog.showDialog(error.responseText);
@@ -104,18 +123,18 @@ var NavBar = {
         });
     },
 
-    getRankBar: function() {
+    getRankBar: function () {
         var userRank = Utils.getUserRank();
         var barValue;
-        barValue = Math.max(0,userRank) + 35;
-        barValue = Math.min(100,barValue);
+        barValue = Math.max(0, userRank) + 35;
+        barValue = Math.min(100, barValue);
         var rank_color = Utils.getRankColor();
         if (rank_color != 'red') {
             return $(
                 '<div class="progress progress-rank">' +
                 '<div class="progress-bar ' + rank_color + '-gifter" role="progressbar" aria-valuenow="' + barValue + '"' +
                 'aria-valuemin="0" aria-valuemax="100" style="width:' + barValue + '%">' +
-                    'Rank: ' + userRank +
+                'Rank: ' + userRank +
                 '</div>' +
                 '</div>'
             );
@@ -125,7 +144,7 @@ var NavBar = {
                 '<div class="progress progress-rank">' +
                 '<div class="progress-bar red-gifter" role="progressbar" aria-valuenow="100"' +
                 'aria-valuemin="0" aria-valuemax="100" style="width:100%">' +
-                    'User under warning' +
+                'User under warning' +
                 '</div>' +
                 '</div>'
             );
--- owls/gifted/static/gifted/views/main/Views.js
+++ owls/gifted/static/gifted/views/main/Views.js
@@ -283,6 +283,25 @@ var FAQView = {
     showFAQView: function () {
         Utils.clearView('.main-container');
         var FAQPage = 'static/gifted/inner-templates/faqPage.html';
-        Utils.injectView('.main-container', FAQPage, null, 'background1_80.jpg');
+        Utils.injectView('.main-container', FAQPage, FAQView.onFAQInjected, 'background1_80.jpg');
+    },
+
+
+
+    onFAQInjected: function () {
+        $('.faq_question').click(function () {
+
+            if ($(this).parent().is('.open')) {
+                $(this).closest('.faq').find('.faq_answer_container').animate({'height': '0'}, 500);
+                $(this).closest('.faq').removeClass('open');
+
+            } else {
+                var newHeight = $(this).closest('.faq').find('.faq_answer').height() + 'px';
+                $(this).closest('.faq').find('.faq_answer_container').animate({'height': newHeight}, 500);
+                $(this).closest('.faq').addClass('open');
+            }
+
+        });
     }
+
 };
